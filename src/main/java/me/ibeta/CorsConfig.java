7a93760fcfe6b10b23c9b45812c19a5a45f6a048
Sunbelife <Sunbelife@foxmail.com>
2018-10-03 15:22:53 +0800

    Add CROS Support

---
+++ src/main/java/me/ibeta/CorsConfig.java
@@ -0,0 +1,22 @@
+package me.ibeta;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * 解决前端站点(主要为JavaScript发起的Ajax请求)访问的跨域问题
+ */
+@Configuration
+public class CorsConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("*")    //允许所有前端站点调用
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .maxAge(1728000);
+    }
+}
